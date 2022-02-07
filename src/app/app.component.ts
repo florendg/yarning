803956e68d354c7d65f9ee28a7545959a8291bdd
floren <florendg@gmail.com>
2022-02-07 21:25:12 +0100

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Card } from "./model/card";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  task: Card = {
+    title: 'Task 1',
+    description: 'This is a demo card'
+  };
+
+  tasks: Card[] = [];
+
   title = 'yarning';
+
+  ngOnInit(): void {
+    this.tasks.push(this.task);
+    this.tasks.push({
+      title: 'Second Card',
+      description: 'A Second card'
+    });
+  }
+
+
 }
